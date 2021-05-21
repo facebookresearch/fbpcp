290f5ba8fe5c48cb800b62d71dd72880ada4a3d6
Sidhesh Desai <sidhesh@fb.com>
2021-05-21 13:02:15 -0700

    Added error handling to FBPCS gateways (#15)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/FBPCS/pull/15
    
    Added error_handler decorator to each function in cloudwatch, ec2, s3 files
    
    Reviewed By: peking2, zhuang-93
    
    Differential Revision: D28548416
    
    fbshipit-source-id: 4d9bbd4101259b75b840ec3124cb15b1f2c027d4

--- fbpcs/gateway/s3.py
+++ fbpcs/gateway/s3.py
@@ -10,6 +10,7 @@ import os
 from typing import Any, Dict, List, Optional
 
 import boto3
+from fbpcs.decorator.error_handler import error_handler
 from tqdm.auto import tqdm
 
 
@@ -33,15 +34,18 @@ class S3Gateway:
         # pyre-ignore
         self.client = boto3.client("s3", region_name=self.region, **config)
 
+    @error_handler
     def create_bucket(self, bucket: str, region: Optional[str] = None) -> None:
         region = region if region is not None else self.region
         self.client.create_bucket(
             Bucket=bucket, CreateBucketConfiguration={"LocationConstraint": region}
         )
 
+    @error_handler
     def delete_bucket(self, bucket: str) -> None:
         self.client.delete_bucket(Bucket=bucket)
 
+    @error_handler
     def upload_file(self, file_name: str, bucket: str, key: str) -> None:
         file_size = os.path.getsize(file_name)
         self.client.upload_file(
@@ -51,6 +55,7 @@ class S3Gateway:
             Callback=self.ProgressPercentage(file_name, file_size),
         )
 
+    @error_handler
     def download_file(self, bucket: str, key: str, file_name: str) -> None:
         file_size = self.get_object_size(bucket, key)
         self.client.download_file(
@@ -60,19 +65,24 @@ class S3Gateway:
             Callback=self.ProgressPercentage(file_name, file_size),
         )
 
+    @error_handler
     def put_object(self, bucket: str, key: str, data: str) -> None:
         self.client.put_object(Bucket=bucket, Key=key, Body=data.encode())
 
+    @error_handler
     def get_object(self, bucket: str, key: str) -> str:
         res = self.client.get_object(Bucket=bucket, Key=key)
         return res["Body"].read().decode()
 
+    @error_handler
     def get_object_size(self, bucket: str, key: str) -> int:
         return self.client.head_object(Bucket=bucket, Key=key)["ContentLength"]
 
+    @error_handler
     def get_object_info(self, bucket: str, key: str) -> Dict[str, Any]:
         return self.client.get_object(Bucket=bucket, Key=key)
 
+    @error_handler
     def list_object2(self, bucket: str, key: str) -> List[str]:
         paginator = self.client.get_paginator("list_objects_v2")
         pages = paginator.paginate(Bucket=bucket, Prefix=key)
@@ -84,9 +94,11 @@ class S3Gateway:
 
         return key_list
 
+    @error_handler
     def delete_object(self, bucket: str, key: str) -> None:
         self.client.delete_object(Bucket=bucket, Key=key)
 
+    @error_handler
     def object_exists(self, bucket: str, key: str) -> bool:
         try:
             # Result intentionally discarded
@@ -95,6 +107,7 @@ class S3Gateway:
         except Exception:
             return False
 
+    @error_handler
     def copy(
         self, source_bucket: str, source_key: str, dest_bucket: str, dest_key: str
     ) -> None:
