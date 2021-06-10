8ef204498f8311d0703fd907112f91e5c7143fe6
Sidhesh Desai <sidhesh@fb.com>
2021-06-10 10:09:11 -0700

    Removed Error Suppression with DataClassJsonMixin
    Summary: Made OWDLState and OWDLWorkflow subclasses of DataClassJsonMixin to avoid error suppression
    
    Reviewed By: peking2
    
    Differential Revision: D29031271
    
    fbshipit-source-id: f981835fca22e270abfd379628d5a52be99dbe65

--- onedocker/onedocker_lib/entity/owdl_state.py
+++ onedocker/onedocker_lib/entity/owdl_state.py
@@ -9,12 +9,11 @@
 from dataclasses import dataclass, field
 from typing import List, Optional
 
-from dataclasses_json import config, dataclass_json
+from dataclasses_json import config, DataClassJsonMixin
 
 
-@dataclass_json
 @dataclass
-class OWDLState:
+class OWDLState(DataClassJsonMixin):
     type_: str = field(metadata=config(field_name="Type"))
     container_definition: str = field(metadata=config(field_name="ContainerDefinition"))
     package_name: str = field(metadata=config(field_name="PackageName"))
@@ -45,5 +44,4 @@ class OWDLState:
         self.version = version
 
     def __str__(self) -> str:
-        # pyre-ignore
         return self.to_json()
