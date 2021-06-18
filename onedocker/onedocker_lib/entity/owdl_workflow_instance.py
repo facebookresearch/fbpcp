cc7989d39193ca55397fd30f4ad748f8c9bf1a08
Sidhesh Desai <sidhesh@fb.com>
2021-06-18 09:06:59 -0700

    Added OWDL Workflow Instance Entity (#44)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/FBPCS/pull/44
    
    Created OWDL Workflow Instance Entity to represent Workflows that are currently running
    
    Reviewed By: peking2
    
    Differential Revision: D29142869
    
    fbshipit-source-id: 225051e4d8c53002eaa9601a353fe2c7104e1afb

---
+++ onedocker/onedocker_lib/entity/owdl_workflow_instance.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+from dataclasses import dataclass
+from enum import Enum
+from typing import List
+
+from dataclasses_json import DataClassJsonMixin
+from onedocker.onedocker_lib.entity.owdl_state_instance import OWDLStateInstance
+from onedocker.onedocker_lib.entity.owdl_workflow import OWDLWorkflow
+
+
+class Status(Enum):
+    CREATED = "CREATED"
+    STARTED = "STARTED"
+    FAILED = "FAILED"
+    COMPLETED = "COMPLETED"
+
+
+@dataclass
+class OWDLWorkflowInstance(DataClassJsonMixin):
+    owdl_workflow: OWDLWorkflow
+    state_instances: List[OWDLStateInstance]
+    status: Status = Status.CREATED
+
+    def get_current_state_inst(self) -> OWDLStateInstance:
+        return self.state_instances[-1]
+
+    def add_next_state_inst(self, state_inst: OWDLStateInstance) -> None:
+        self.state_instances.append(state_inst)
+
+    def __str__(self) -> str:
+        return self.to_json()
