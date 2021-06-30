533eec6a557543dcad366490241f4ec3f4a3d38a
Gary Zhao <garyzhao@fb.com>
2021-06-30 10:07:17 -0700

    Cast type (#64)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/FBPCS/pull/64
    
    This is a followup diff for D29447049. It's to cast StorageService to S3StorageService.
    
    Reviewed By: wenhaizhu
    
    Differential Revision: D29479684
    
    fbshipit-source-id: 5d1b924a4a26bd398bb9cdb9a06a4b57a2640535

--- fbpcs/util/typing.py
+++ fbpcs/util/typing.py
@@ -13,9 +13,7 @@ T = TypeVar("T")
 V = TypeVar("V")
 
 
-# pyre-fixme[34]: `T` isn't present in the function's parameters.
-def checked_cast(typ: Type[V], val: V) -> T:
+def checked_cast(typ: Type[T], val: V) -> T:
     if not isinstance(val, typ):
         raise ValueError(f"Value was not of type {type!r}:\n{val!r}")
-    # pyre-fixme[7]: Expected `T` but got `V`.
     return val
