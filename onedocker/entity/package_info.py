790eb952665625dd5cfbc59dfd46b30e7f0b45fd
Ziyan Huang <ziyanhuang1993@fb.com>
2021-08-17 23:38:54 -0700

    OSS onedocker package repository (#123)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcp/pull/123
    
    We want to oss onedocker package repository so our E2E scripts can upload binaries to S3 through onedocker_cli instead of aws cli, which depends on onedocker package repository.
    
    1. Moved onedocker package repository to oss folder.
    2. Renamed onedocker-common to onedocker (it now includes common, entity and repository)
    
    Reviewed By: peking2, jliu0501
    
    Differential Revision: D30373547
    
    fbshipit-source-id: f937406eb51e24c4e472e42a7c1f2973504c1fe2

---
+++ onedocker/entity/package_info.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+from dataclasses import dataclass
+
+
+@dataclass
+class PackageInfo:
+    package_name: str
+    version: str
+    last_modified: str
+    package_size: int
