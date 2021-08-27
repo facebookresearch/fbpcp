9c15268686160a1acece5a64e6aaa1bd78bc6071
Ji Liu <jliu0501@fb.com>
2021-08-27 09:31:07 -0700

    Add BillingService and AWSBillingService (#137)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcp/pull/137
    
    Adding BillingService as abstract class for billing service and AWSBillingService as the implementation class for AWS
    
    Reviewed By: peking2
    
    Differential Revision: D30556918
    
    fbshipit-source-id: f8cf4346c5d0847c8c0882744f9f95e78e4bad0a

---
+++ fbpcp/service/billing.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+import abc
+from datetime import date
+
+from fbpcp.entity.cloud_cost import CloudCost
+
+
+class BillingService(abc.ABC):
+    @abc.abstractmethod
+    def get_cost(self, start_date: date, end_date: date) -> CloudCost:
+        pass
