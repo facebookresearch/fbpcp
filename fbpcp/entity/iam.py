29a917830047271a90af2aa67f8b7beb0ba7722b
Zach Li <zehuali@fb.com>
2021-08-30 10:21:40 -0700

    Add IAM entity
    Differential Revision: D30633507
    
    fbshipit-source-id: 14c19502a5da6252f4681ea27dae0b4d944828f0

---
+++ fbpcp/entity/iam.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+from dataclasses import dataclass, field
+from typing import Dict, List
+
+
+@dataclass
+class Policy:
+    name: str
+    resource_name: str
+    policy_json: str
+    permissions: List[str]
+
+
+@dataclass
+class Role:
+    name: str
+    resource_name: str
+    assume_role_policy_json: str
+    attached_policies: List[Policy]
+    tags: Dict[str, str] = field(default_factory=lambda: {})
