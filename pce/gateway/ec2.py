84a8b1918352d38f20b70d7fca9e30bea2362ba1
Hiram Ernesto Alvarado Gaspar <ernestoalvarado@fb.com>
2021-11-11 16:49:04 -0800

    Create IAM gateway (#195)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcp/pull/195
    
    Add a IAM gateway to retrieve policy documents attached to the roles the PCE container point to.
    
    Reviewed By: zehuali
    
    Differential Revision: D32144365
    
    fbshipit-source-id: af5e96b3626f62a656111f97db3935eb00d510d8

--- pce/gateway/ec2.py
+++ pce/gateway/ec2.py
@@ -3,13 +3,15 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import logging
+# pyre-strict
+
 from typing import List, Optional, Dict, Any
 
 import boto3
+from fbpcp.gateway.aws import AWSGateway
 
 
-class EC2Gateway:
+class EC2Gateway(AWSGateway):
     def __init__(
         self,
         region: str,
@@ -17,16 +19,9 @@ class EC2Gateway:
         access_key_data: Optional[str] = None,
         config: Optional[Dict[str, Any]] = None,
     ) -> None:
-        self.logger: logging.Logger = logging.getLogger(__name__)
-        self.region = region
-        self.config: Dict[str, Any] = config or {}
+        super().__init__(region, access_key_id, access_key_data, config)
 
-        if access_key_id is not None:
-            self.config["aws_access_key_id"] = access_key_id
-
-        if access_key_data is not None:
-            self.config["aws_secret_access_key"] = access_key_data
-
+        # pyre-ignore
         self.client = boto3.client("ec2", region_name=self.region, **self.config)
 
     def describe_availability_zones(
