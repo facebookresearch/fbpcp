aab5e840f844cc30048d545686168a9a65ee9517
Yutong Wang <yutongwang@fb.com>
2021-11-16 10:50:16 -0800

    Add ec2_gateway/ecs_gateway to FBAWSPCEService [5/n] (#199)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcp/pull/199
    
    Add FBS3Gateway and FBEC2Gateway to FBAWSPCEService
    
    Reviewed By: zehuali
    
    Differential Revision: D32409744
    
    fbshipit-source-id: 499fdd2e58eaecefb456f85a8cde87514ef8c94d

--- fbpcp/service/pce_aws.py
+++ fbpcp/service/pce_aws.py
@@ -31,9 +31,8 @@ class AWSPCEService(PCEService):
     ) -> None:
         self.logger: logging.Logger = logging.getLogger(__name__)
         self.region = region
-        self.access_key_id = access_key_id
-        self.access_key_data = access_key_data
-        self.config = config
+        self.ec2_gateway = EC2Gateway(region, access_key_id, access_key_data, config)
+        self.ecs_gateway = ECSGateway(region, access_key_id, access_key_data, config)
 
     def get_pce(
         self,
@@ -44,19 +43,17 @@ class AWSPCEService(PCEService):
         return PCE(pce_id, self.region, pce_network, pce_compute)
 
     def _get_network(self, pce_id: str) -> PCENetwork:
-        ec2_gateway = EC2Gateway(
-            self.region, self.access_key_id, self.access_key_data, self.config
-        )
+
         tags = {PCE_ID_KEY: pce_id}
-        vpcs = ec2_gateway.describe_vpcs(tags=tags)
+        vpcs = self.ec2_gateway.describe_vpcs(tags=tags)
         vpc = vpcs[0] if vpcs else None
-        subnets = ec2_gateway.describe_subnets(tags=tags)
-        firewall_rulesets = ec2_gateway.describe_security_groups(tags=tags)
-        route_tables = ec2_gateway.describe_route_tables(tags=tags)
+        subnets = self.ec2_gateway.describe_subnets(tags=tags)
+        firewall_rulesets = self.ec2_gateway.describe_security_groups(tags=tags)
+        route_tables = self.ec2_gateway.describe_route_tables(tags=tags)
         route_table = route_tables[0] if route_tables else None
         vpc_peering = None
         if vpc:
-            vpc_peerings = ec2_gateway.describe_vpc_peerings(
+            vpc_peerings = self.ec2_gateway.describe_vpc_peerings(
                 vpc_id=vpc.vpc_id, tags=tags
             )
             vpc_peering = vpc_peerings[0] if vpc_peerings else None
@@ -70,17 +67,14 @@ class AWSPCEService(PCEService):
         )
 
     def _get_compute(self, pce_id: str) -> PCECompute:
-        ecs_gateway = ECSGateway(
-            self.region, self.access_key_id, self.access_key_data, self.config
-        )
         tags = {PCE_ID_KEY: pce_id}
-        clusters = ecs_gateway.describe_clusters(tags=tags)
+        clusters = self.ecs_gateway.describe_clusters(tags=tags)
         cluster = clusters[0] if clusters else None
-        container_definitions = ecs_gateway.describe_task_definitions(tags=tags)
+        container_definitions = self.ecs_gateway.describe_task_definitions(tags=tags)
         if container_definitions:
             container_definition = container_definitions[0]
         else:
-            container_definition = ecs_gateway.describe_task_definition(
+            container_definition = self.ecs_gateway.describe_task_definition(
                 SHARED_TASK_DEFINITION_PREFIX + self.region
             )
         return PCECompute(self.region, cluster, container_definition)
