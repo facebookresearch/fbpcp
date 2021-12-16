b23abd50000ef4fbe4dcc9ec6a318ae79813a5a2
Yaroslav Bulatov <yaroslavvb@fb.com>
2021-12-16 10:21:16 -0800

    Add missing return type annotations
    Summary: Add missing return type annotations
    
    Differential Revision: D33151192
    
    fbshipit-source-id: a8c221a55d6a6b7e00686ed1de13c62a1d4de9ed

--- tests/util/test_s3path.py
+++ tests/util/test_s3path.py
@@ -10,13 +10,13 @@ from fbpcp.util.s3path import S3Path
 
 
 class TestS3Path(unittest.TestCase):
-    def test_s3path_no_subfolder(self):
+    def test_s3path_no_subfolder(self) -> None:
         test_s3path = S3Path("https://bucket-name.s3.Region.amazonaws.com/key-name")
         self.assertEqual(test_s3path.region, "Region")
         self.assertEqual(test_s3path.bucket, "bucket-name")
         self.assertEqual(test_s3path.key, "key-name")
 
-    def test_s3path_with_subfoler(self):
+    def test_s3path_with_subfoler(self) -> None:
         test_s3path = S3Path(
             "https://bucket-name.s3.Region.amazonaws.com/subfolder/key"
         )
@@ -24,7 +24,7 @@ class TestS3Path(unittest.TestCase):
         self.assertEqual(test_s3path.bucket, "bucket-name")
         self.assertEqual(test_s3path.key, "subfolder/key")
 
-    def test_s3path_invalid_fileURL(self):
+    def test_s3path_invalid_fileURL(self) -> None:
         test_url = "an invalid fileURL"
         with self.assertRaises(ValueError):
             S3Path(test_url)
