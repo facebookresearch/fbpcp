097988da7e20c7972d1a5675382670235a0d358d
Xiaomeng Shen <xshen10@fb.com>
2022-01-20 07:22:34 -0800

    add Pyre Missing Annotation in test_storage.py (#236)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcp/pull/236
    
    added None return type to all 3 functions
    
    Reviewed By: corbantek
    
    Differential Revision: D33614936
    
    fbshipit-source-id: 5893a3281f19cccef1037f659b575cd18d07e8e3

--- tests/service/test_storage.py
+++ tests/service/test_storage.py
@@ -10,18 +10,18 @@ from fbpcp.service.storage import PathType, StorageService
 
 
 class TestStorageService(unittest.TestCase):
-    def test_path_type_s3(self):
+    def test_path_type_s3(self) -> None:
         type_ = StorageService.path_type(
             "https://bucket-name.s3.Region.amazonaws.com/key-name"
         )
         self.assertEqual(type_, PathType.S3)
 
-    def test_path_type_gcs(self):
+    def test_path_type_gcs(self) -> None:
         type_ = StorageService.path_type(
             "https://storage.cloud.google.com/bucket-name/key-name"
         )
         self.assertEqual(type_, PathType.GCS)
 
-    def test_path_type_local(self):
+    def test_path_type_local(self) -> None:
         type_ = StorageService.path_type("/usr/file")
         self.assertEqual(type_, PathType.Local)
