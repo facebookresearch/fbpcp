34002e0aa68794ee4e4000241105f55eb6ce315b
Matthew Barr <matthewbarr@fb.com>
2022-04-27 10:32:26 -0700

    Output user arn when runing PCE validator (#301)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcp/pull/301
    
    Add code to output the user ARN when running PCE validator
    
    Differential Revision: D35940106
    
    fbshipit-source-id: 78681d733f0f174f6e7be481bf4f5ae8f4a55260

---
+++ pce/gateway/sts.py
@@ -0,0 +1,32 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+from typing import Optional, Dict, Any
+
+import boto3
+from fbpcp.gateway.aws import AWSGateway
+
+
+class STSGateway(AWSGateway):
+    def __init__(
+        self,
+        region: str,
+        access_key_id: Optional[str] = None,
+        access_key_data: Optional[str] = None,
+        config: Optional[Dict[str, Any]] = None,
+    ) -> None:
+        super().__init__(region, access_key_id, access_key_data, config)
+
+        # pyre-ignore
+        self.client = boto3.client("sts", region_name=self.region, **self.config)
+
+    def get_caller_arn(
+        self,
+    ) -> str:
+        response = self.client.get_caller_arn()
+
+        return response["Arn"]
