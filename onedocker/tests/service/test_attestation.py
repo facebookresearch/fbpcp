5903a779ae38da67682442e45962efe23b9ce18b
Arnav Sacheti <asacheti@fb.com>
2022-05-31 15:36:04 -0700

    Checksum Upload (#318)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcp/pull/318
    
    Extends work conducted by previous diff to upload the generated checksums into designated checksum repo/bucket
    
    Reviewed By: ajaybhargavb
    
    Differential Revision: D36560013
    
    fbshipit-source-id: 60a5f904a1fe3dd68ba596fe1dc6f245b81464c6

---
+++ onedocker/tests/service/test_attestation.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import unittest
+from unittest.mock import patch
+
+from fbpcp.service.storage_s3 import S3StorageService
+from onedocker.entity.checksum_type import ChecksumType
+from onedocker.service.attestation import AttestationService
+from onedocker.service.checksum import LocalChecksumGenerator
+
+
+class TestAttestationService(unittest.TestCase):
+    @patch.object(LocalChecksumGenerator, "generate_checksums")
+    @patch.object(S3StorageService, "write")
+    def test_track_package_s3(
+        self, MockS3StorageServiceWrite, MockLocalChecksumGeneratorGenerateChecksum
+    ):
+        # Arrange
+        repository_path = (
+            "https://onedocker-runner-unittest-asacheti.s3.us-west-2.amazonaws.com/"
+        )
+        attestation_service = AttestationService(
+            S3StorageService("us-west-2"),
+            repository_path,
+        )
+
+        algorithms = [ChecksumType.MD5, ChecksumType.SHA256, ChecksumType.BLAKE2B]
+
+        test_path = "/usr/bin/ls"
+        test_package_name = "ls"
+        test_version = "latest"
+
+        MockLocalChecksumGeneratorGenerateChecksum.return_value = {
+            "MD5": "valid_md5_checksum_goes_here",
+            "SHA256": "valid_sha256_checksum_goes_here",
+            "BLAKE2B": "valid_blake2b_checksum_goes_here",
+        }
+
+        expected_file_contents = (
+            "{\n"
+            + '    "Package Name": "ls",\n'
+            + '    "Package Version": "latest",\n'
+            + '    "Checksums": {\n'
+            + '        "MD5": "valid_md5_checksum_goes_here",\n'
+            + '        "SHA256": "valid_sha256_checksum_goes_here",\n'
+            + '        "BLAKE2B": "valid_blake2b_checksum_goes_here"\n'
+            + "    }\n"
+            + "}"
+        )
+        expected_file_path = f"{repository_path}ls/latest.json"
+
+        # Act
+        attestation_service.track_binary(
+            binary_path=test_path,
+            package_name=test_package_name,
+            version=test_version,
+        )
+
+        # Assert
+        MockLocalChecksumGeneratorGenerateChecksum.assert_called_once_with(
+            path_to_binary=test_path,
+            checksum_algorithms=algorithms,
+        )
+        MockS3StorageServiceWrite.assert_called_once_with(
+            expected_file_path,
+            expected_file_contents,
+        )
