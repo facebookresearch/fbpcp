a4d3e6c72e9148d99f3e54ebb3dbb9128c9da23c
Arnav Sacheti <asacheti@fb.com>
2022-07-14 11:53:14 -0700

    Adding support for plaintext encryption
    Differential Revision: D37838138
    
    fbshipit-source-id: b96206c6db7fc56a3b9463802b17dc0326433ca0

--- fbpcp/service/key_managment_aws.py
+++ fbpcp/service/key_managment_aws.py
@@ -15,6 +15,7 @@ from fbpcp.service.key_management import KeyManagmentService
 
 class AWSKeyManagmentService(KeyManagmentService):
     key_id: str
+    encryption_algorithm: str
     signing_algorithm: str
     grant_tokens: List[str]
 
@@ -22,14 +23,22 @@ class AWSKeyManagmentService(KeyManagmentService):
         self,
         region: str,
         key_id: str,
+        encryption_algorithm: Optional[str] = None,
         signing_algorithm: Optional[str] = None,
         grant_tokens: Optional[List[str]] = None,
         access_key_id: Optional[str] = None,
         access_key_data: Optional[str] = None,
         config: Optional[Dict[str, Any]] = None,
     ) -> None:
-        self.kms_gateway = KMSGateway(region, access_key_id, access_key_data, config)
+        self.kms_gateway = KMSGateway(
+            region=region,
+            access_key_id=access_key_id,
+            access_key_data=access_key_data,
+            config=config,
+        )
         self.key_id = key_id
+
+        self.encryption_algorithm = encryption_algorithm if encryption_algorithm else ""
         self.signing_algorithm = signing_algorithm if signing_algorithm else ""
         self.grant_tokens = grant_tokens if grant_tokens else []
 
@@ -45,3 +54,18 @@ class AWSKeyManagmentService(KeyManagmentService):
         )
         signature = b64encode(response["Signature"]).decode()
         return signature
+
+    def encrypt(
+        self, plaintext: str, encryption_context: Optional[Dict[str, str]] = None
+    ) -> str:
+        if not self.encryption_algorithm:
+            raise ValueError("No Encryption Algorithm Set")
+        response = self.kms_gateway.encrypt(
+            key_id=self.key_id,
+            plaintext=plaintext,
+            encryption_context=encryption_context if encryption_context else {},
+            grant_tokens=self.grant_tokens,
+            encryption_algorithm=self.encryption_algorithm,
+        )
+        ciphertext_blob = b64encode(response["CiphertextBlob"]).decode()
+        return ciphertext_blob
