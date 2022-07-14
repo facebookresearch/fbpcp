3c1b6b3c099cc114173902bb08d3d5562087093c
Arnav Sacheti <asacheti@fb.com>
2022-07-14 11:52:13 -0700

    Integrate into onedocker-runner cli (#344)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcp/pull/344
    
    # Context
    What good is integration if no easy way to interact when running program from CLI
    # This commit
    Adds default and env variable lookup allowing run from CLI
    
    Differential Revision: D37229577
    
    fbshipit-source-id: 3bdada7c6e31ea6166c6343c5e1d936254311984

--- onedocker/common/env.py
+++ onedocker/common/env.py
@@ -7,8 +7,14 @@
 # This is the repository path that OneDocker downloads binaries from
 ONEDOCKER_REPOSITORY_PATH = "ONEDOCKER_REPOSITORY_PATH"
 
+# This is the repository path that OneDocker downloads binary checksums from
+ONEDOCKER_CHECKSUM_REPOSITORY_PATH = "ONEDOCKER_CHECKSUM_REPOSITORY_PATH"
+
 # This is the local path that the binaries reside
 ONEDOCKER_EXE_PATH = "ONEDOCKER_EXE_PATH"
 
 # This is the path user specified to upload the core dump file to
 CORE_DUMP_REPOSITORY_PATH = "CORE_DUMP_REPOSITORY_PATH"
+
+# This is the type of checksum we want to compare when running program
+ONEDOCKER_CHECKSUM_TYPE = "ONEDOCKER_CHECKSUM_TYPE"
