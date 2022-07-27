1230772f5262b22b204c0ffb4968012c8015b981
Arnav Sacheti <asacheti@fb.com>
2022-07-27 14:23:27 -0700

    Creating KMS gateway and service (#366)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcp/pull/366
    
    # What are Grant Tokens
    https://docs.aws.amazon.com/kms/latest/developerguide/grants.html
    
    Reviewed By: ziqih
    
    Differential Revision: D37400919
    
    fbshipit-source-id: 832a3756fb52ac12438449776a426c63db80f0b2

---
+++ fbpcp/gateway/kms.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+from typing import Any, Dict, Optional
+
+import boto3
+from botocore.client import BaseClient
+from fbpcp.gateway.aws import AWSGateway
+
+
+class KMSGateway(AWSGateway):
+    def __init__(
+        self,
+        region: str,
+        access_key_id: Optional[str] = None,
+        access_key_data: Optional[str] = None,
+        config: Optional[Dict[str, Any]] = None,
+    ) -> None:
+        super().__init__(region, access_key_id, access_key_data, config)
+        self.client: BaseClient = boto3.client(
+            "kms", region_name=self.region, **self.config
+        )
