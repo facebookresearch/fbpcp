ba07a8b8195d4eb533a5fbe522ca4d8fc0045aa0
Ziqi Huang <ziqihuang@fb.com>
2022-08-19 13:15:09 -0700

    Create ContainerType entity (#413)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcp/pull/413
    
    Create ContainerType entity to specify cpu and memory for different container types; This will be used by ContainerService in later diffs
    
    Reviewed By: zhuang-93
    
    Differential Revision: D38724061
    
    fbshipit-source-id: 76d97f6697fd5907a8c3e7cc9af0b3ce9bea08c3

---
+++ fbpcp/entity/container_type.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+from dataclasses import dataclass
+from enum import Enum
+from typing import Dict
+
+from fbpcp.entity.cloud_provider import CloudProvider
+from fbpcp.error.pcp import InvalidParameterError
+
+
+class ContainerType(Enum):
+    DEFAULT = "DEFAULT"
+    LARGE = "LARGE"
+
+
+CONTAINER_TYPES: Dict[CloudProvider, Dict[ContainerType, Dict[str, int]]] = {
+    CloudProvider.AWS: {
+        ContainerType.DEFAULT: {"cpu": 4, "memory": 30},
+        ContainerType.LARGE: {"cpu": 16, "memory": 120},
+    }
+}
+
+
+@dataclass
+class ContainerTypeConfig:
+    cpu: int  # Number of vCPU
+    memory: int  # Memory in GB
+
+    @classmethod
+    def get_config(
+        cls, cloud_provider: CloudProvider, container_type: ContainerType
+    ) -> "ContainerTypeConfig":
+        if cloud_provider != CloudProvider.AWS:
+            raise InvalidParameterError(
+                f"Cloud provider {cloud_provider} is not supported."
+            )
+        return cls(**CONTAINER_TYPES[cloud_provider][container_type])
