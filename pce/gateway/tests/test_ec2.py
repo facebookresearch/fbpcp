5c9eb6c9bb4c88e1f67caaeb2082fe3e40107b9c
Michael Meng <kewen12@meta.com>
2022-10-07 14:26:50 -0700

    Unit tests for the accept function (#447)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcp/pull/447
    
    Unit test for `accept_vpc_peering_connection` function.
    
    Reviewed By: ajaybhargavb
    
    Differential Revision: D40123538
    
    fbshipit-source-id: 90299fe03d0dfcfa8a3690177ce2509baae99368

--- pce/gateway/tests/test_ec2.py
+++ pce/gateway/tests/test_ec2.py
@@ -4,29 +4,48 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-# pyre-strict
-
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 
+from botocore.exceptions import ClientError
+
 from fbpcp.entity.vpc_peering import VpcPeering, VpcPeeringRole, VpcPeeringState
+from fbpcp.error.pcp import PcpError
 from pce.gateway.ec2 import EC2Gateway
+
+TEST_REGION = "us-west-2"
+TEST_AWS_ACCOUNT_ID = "123456789012"
+TEST_ACCEPTER_VPC_ID = "vpc-12345"
+TEST_REQUESTER_VPC_ID = "vpc-56789"
+TEST_VPC_PEERING_ID = "pcx-77889900"
+TEST_ACCEPTER_CIDR_BLOCK = "10.0.0.0/16"
+TEST_REQUESTER_CIDR_BLOCK = "10.1.0.0/16"
+TEST_VPC_PEERING_CONNECTION = {
+    "AccepterVpcInfo": {
+        "CidrBlock": TEST_ACCEPTER_CIDR_BLOCK,
+        "CidrBlockSet": [{"CidrBlock": "10.0.0.0/16"}],
+        "OwnerId": TEST_AWS_ACCOUNT_ID,
+        "VpcId": TEST_ACCEPTER_VPC_ID,
+        "TEST_REGION": TEST_REGION,
+    },
+    "RequesterVpcInfo": {
+        "CidrBlock": TEST_REQUESTER_CIDR_BLOCK,
+        "CidrBlockSet": [{"CidrBlock": "10.1.0.0/16"}],
+        "OwnerId": TEST_AWS_ACCOUNT_ID,
+        "VpcId": TEST_REQUESTER_VPC_ID,
+        "TEST_REGION": TEST_REGION,
+    },
+    "Status": {"Code": "active", "Message": "Active"},
+    "VpcPeeringConnectionId": TEST_VPC_PEERING_ID,
+}
 
 
 class TestEC2Gateway(TestCase):
-    REGION = "us-west-2"
-    TEST_AWS_ACCOUNT_ID = "123456789012"
-    TEST_ACCEPTER_VPC_ID = "vpc-12345"
-    TEST_REQUESTER_VPC_ID = "vpc-56789"
-    TEST_VPC_PEERING_ID = "pcx-77889900"
-    TEST_ACCEPTER_CIDR_BLOCK = "10.0.0.0/16"
-    TEST_REQUESTER_CIDR_BLOCK = "10.1.0.0/16"
-
     def setUp(self) -> None:
         self.aws_ec2 = MagicMock()
         with patch("boto3.client") as mock_client:
             mock_client.return_value = self.aws_ec2
-            self.ec2 = EC2Gateway(self.REGION)
+            self.ec2 = EC2Gateway(TEST_REGION)
 
     def test_describe_availability_zones(self) -> None:
         # Arrange
@@ -71,45 +90,26 @@ class TestEC2Gateway(TestCase):
     def test_describe_vpc_peering_connections_with_accepter_vpc_id(self) -> None:
         # Arrange
         client_return_response = {
-            "VpcPeeringConnections": [
-                {
-                    "AccepterVpcInfo": {
-                        "CidrBlock": self.TEST_ACCEPTER_CIDR_BLOCK,
-                        "CidrBlockSet": [{"CidrBlock": "10.0.0.0/16"}],
-                        "OwnerId": self.TEST_AWS_ACCOUNT_ID,
-                        "VpcId": self.TEST_ACCEPTER_VPC_ID,
-                        "Region": self.REGION,
-                    },
-                    "RequesterVpcInfo": {
-                        "CidrBlock": self.TEST_REQUESTER_CIDR_BLOCK,
-                        "CidrBlockSet": [{"CidrBlock": "10.1.0.0/16"}],
-                        "OwnerId": self.TEST_AWS_ACCOUNT_ID,
-                        "VpcId": self.TEST_REQUESTER_VPC_ID,
-                        "Region": self.REGION,
-                    },
-                    "Status": {"Code": "active", "Message": "Active"},
-                    "VpcPeeringConnectionId": self.TEST_VPC_PEERING_ID,
-                }
-            ],
+            "VpcPeeringConnections": [TEST_VPC_PEERING_CONNECTION],
         }
         self.aws_ec2.describe_vpc_peering_connections = MagicMock(
             return_value=client_return_response
         )
 
         expected_vpc_peering = VpcPeering(
-            id=self.TEST_VPC_PEERING_ID,
+            id=TEST_VPC_PEERING_ID,
             status=VpcPeeringState.ACTIVE,
             role=VpcPeeringRole.ACCEPTER,
-            requester_vpc_id=self.TEST_REQUESTER_VPC_ID,
-            accepter_vpc_id=self.TEST_ACCEPTER_VPC_ID,
-            requester_vpc_cidr=self.TEST_REQUESTER_CIDR_BLOCK,
-            accepter_vpc_cidr=self.TEST_ACCEPTER_CIDR_BLOCK,
+            requester_vpc_id=TEST_REQUESTER_VPC_ID,
+            accepter_vpc_id=TEST_ACCEPTER_VPC_ID,
+            requester_vpc_cidr=TEST_REQUESTER_CIDR_BLOCK,
+            accepter_vpc_cidr=TEST_ACCEPTER_CIDR_BLOCK,
         )
 
         # Act
         vpc_peering_connection = (
             self.ec2.describe_vpc_peering_connections_with_accepter_vpc_id(
-                vpc_id=self.TEST_ACCEPTER_VPC_ID
+                vpc_id=TEST_ACCEPTER_VPC_ID
             )[0]
         )
 
@@ -132,3 +132,53 @@ class TestEC2Gateway(TestCase):
         # Assert
         self.assertIsNone(vpc_peering_connection, "vpc peering connection is None")
         self.aws_ec2.describe_vpc_peering_connections.assert_called
+
+    def test_accept_vpc_peering_connection_success(self) -> None:
+        # Arrange
+        client_return_response = {"VpcPeeringConnection": TEST_VPC_PEERING_CONNECTION}
+        self.aws_ec2.accept_vpc_peering_connection = MagicMock(
+            return_value=client_return_response
+        )
+
+        expected_vpc_peering = VpcPeering(
+            id=TEST_VPC_PEERING_ID,
+            status=VpcPeeringState.ACTIVE,
+            role=VpcPeeringRole.ACCEPTER,
+            requester_vpc_id=TEST_REQUESTER_VPC_ID,
+            accepter_vpc_id=TEST_ACCEPTER_VPC_ID,
+            requester_vpc_cidr=TEST_REQUESTER_CIDR_BLOCK,
+            accepter_vpc_cidr=TEST_ACCEPTER_CIDR_BLOCK,
+        )
+
+        # Act
+        vpc_peering_connection = self.ec2.accept_vpc_peering_connection(
+            vpc_peering_connection_id=TEST_VPC_PEERING_ID, vpc_id=TEST_ACCEPTER_VPC_ID
+        )
+
+        # Assert
+        self.assertEqual(vpc_peering_connection, expected_vpc_peering)
+        self.aws_ec2.accept_vpc_peering_connection.assert_called
+
+    def test_accept_vpc_peering_connection_fail(self) -> None:
+        # Arrange
+        self.aws_ec2.accept_vpc_peering_connection = MagicMock(
+            side_effect=ClientError(
+                {
+                    "Error": {"Message": "test_msg", "Code": "test_code"},
+                    "ResponseMetadata": {},
+                },
+                None,
+            )
+        )
+        # patternlint-disable-next-line f-string-may-be-missing-leading-f
+        expect_msg = "An error occurred (test_code) when calling the None operation: test_msg\n\n Details: {}\n"
+
+        # Act
+        with self.assertRaises(PcpError) as err:
+            self.ec2.accept_vpc_peering_connection(
+                vpc_peering_connection_id=TEST_VPC_PEERING_ID,
+                vpc_id=TEST_ACCEPTER_VPC_ID,
+            )
+
+        # Assert
+        self.assertEqual(str(err.exception), expect_msg)
